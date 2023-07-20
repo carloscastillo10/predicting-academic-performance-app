0d1dd43e1ac133afd1b0d3548a4ac647f71f99b7
Carlos Castillo <carloscastillo090916@gmail.com>
2023-07-20 09:32:48 -0500

    fix(dashboard): :bug: Fix mutistep form

--- frontend/src/components/classify/Form/EnrollmentForm.tsx
+++ frontend/src/components/classify/Form/EnrollmentForm.tsx
@@ -2,9 +2,26 @@
 import React, { useState } from 'react'
 import { enrollmentInputs } from '@variables/forms/enrollment'
 import { Input } from '@components/classify/Form'
+import { Card } from '@components/Card'
+import { ArrowLongRightIcon } from '@heroicons/react/20/solid'
 import styles from '@styles/Checkbox.module.css'
 
-export function EnrollmentForm(): React.JSX.Element {
+const data = {
+  title: {
+    value: 'Datos de Matriculas',
+    size: 'text-lg',
+  },
+  subtitle: {
+    value: 'Información personal de cada estudiante al momento de matricularse',
+    size: 'text-sm',
+  },
+}
+
+interface Props {
+  onNextStepHandler: () => void
+}
+
+export function EnrollmentForm({ onNextStepHandler }: Props): React.JSX.Element {
   const [hasDisability, setHasDisability] = useState(false)
 
   const onHasDisbailityHandler = () => {
@@ -13,71 +30,83 @@ export function EnrollmentForm(): React.JSX.Element {
 
   return (
     <>
-      <div className="mt-6 grid grid-cols-6 gap-x-6 gap-y-8">
-        {enrollmentInputs?.data?.map((props, index) => <Input key={index} {...props} />)}
+      <Card title={data.title} subtitle={data.subtitle}>
+        <div className="mt-6 grid grid-cols-6 gap-x-6 gap-y-8">
+          {enrollmentInputs?.data?.map((props, index) => <Input key={index} {...props} />)}
 
-        <div className={hasDisability ? 'col-span-6 md:col-span-2' : 'col-span-6 md:col-span-4'}>
-          <fieldset>
-            <div className="relative flex gap-x-3">
-              <div className="flex h-6 items-center">
-                <input
-                  type="checkbox"
-                  className={`${styles.checkbox} h-4 w-4 rounded border-gray-300 text-custom focus:ring-custom`}
-                  id="disability"
-                  name="disability"
-                  onChange={onHasDisbailityHandler}
-                />
+          <div className={hasDisability ? 'col-span-6 md:col-span-2' : 'col-span-6 md:col-span-4'}>
+            <fieldset>
+              <div className="relative flex gap-x-3">
+                <div className="flex h-6 items-center">
+                  <input
+                    type="checkbox"
+                    className={`${styles.checkbox} h-4 w-4 rounded border-gray-300 text-custom focus:ring-custom`}
+                    id="disability"
+                    name="disability"
+                    onChange={onHasDisbailityHandler}
+                  />
+                </div>
+                <div className="text-sm leading-6">
+                  <label htmlFor="disability" className="font-bold text-gray-700">
+                    Discapacidad
+                  </label>
+                  <p className="text-gray-500 text-sm">Seleccione si posee algún tipo de discapacidad.</p>
+                </div>
               </div>
-              <div className="text-sm leading-6">
-                <label htmlFor="disability" className="font-bold text-gray-700">
-                  Discapacidad
+            </fieldset>
+          </div>
+
+          {hasDisability && (
+            <>
+              <div className="col-span-6 md:col-span-2">
+                <label htmlFor="disability-percentage" className="block text-sm font-bold leading-6 text-gray-700">
+                  Porcentaje de discapacidad
+                  <span className="ml-1 text-sm text-red-500">*</span>
                 </label>
-                <p className="text-gray-500 text-sm">Seleccione si posee algún tipo de discapacidad.</p>
+                <div className="mt-2">
+                  <input
+                    type="number"
+                    className="block w-full border-0 py-1.5 px-2.5 text-gray-700 caret-custom font-medium text-sm rounded-lg shadow-sm focus:outline-custom ring-1 ring-inset ring-gray-300 leading-6 placeholder:text-gray-400"
+                    id="disability-percentage"
+                    name="disability-percentage"
+                    placeholder="Ej. 50"
+                    min={1}
+                    max={100}
+                  />
+                </div>
               </div>
-            </div>
-          </fieldset>
+
+              <div className="col-span-6 md:col-span-2">
+                <label htmlFor="disabilities-number" className="block text-sm font-bold leading-6 text-gray-700">
+                  Número de discapacidades
+                  <span className="ml-1 text-sm text-red-500">*</span>
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="number"
+                    className="block w-full border-0 py-1.5 px-2.5 text-gray-700 caret-custom font-medium text-sm rounded-lg shadow-sm focus:outline-custom ring-1 ring-inset ring-gray-300 leading-6 placeholder:text-gray-400"
+                    id="disabilities-number"
+                    name="disabilities-number"
+                    placeholder="Ej. 1"
+                    min={1}
+                    max={100}
+                  />
+                </div>
+              </div>
+            </>
+          )}
         </div>
-
-        {hasDisability && (
-          <>
-            <div className="col-span-6 md:col-span-2">
-              <label htmlFor="disability-percentage" className="block text-sm font-bold leading-6 text-gray-700">
-                Porcentaje de discapacidad
-                <span className="ml-1 text-sm text-red-500">*</span>
-              </label>
-              <div className="mt-2">
-                <input
-                  type="number"
-                  className="block w-full border-0 py-1.5 px-2.5 text-gray-700 caret-custom font-medium text-sm rounded-lg shadow-sm focus:outline-custom ring-1 ring-inset ring-gray-300 leading-6 placeholder:text-gray-400"
-                  id="disability-percentage"
-                  name="disability-percentage"
-                  placeholder="Ej. 50"
-                  min={1}
-                  max={100}
-                />
-              </div>
-            </div>
-
-            <div className="col-span-6 md:col-span-2">
-              <label htmlFor="disabilities-number" className="block text-sm font-bold leading-6 text-gray-700">
-                Número de discapacidades
-                <span className="ml-1 text-sm text-red-500">*</span>
-              </label>
-              <div className="mt-2">
-                <input
-                  type="number"
-                  className="block w-full border-0 py-1.5 px-2.5 text-gray-700 caret-custom font-medium text-sm rounded-lg shadow-sm focus:outline-custom ring-1 ring-inset ring-gray-300 leading-6 placeholder:text-gray-400"
-                  id="disabilities-number"
-                  name="disabilities-number"
-                  placeholder="Ej. 1"
-                  min={1}
-                  max={100}
-                />
-              </div>
-            </div>
-          </>
-        )}
-      </div>
+        <div className="mt-10 flex items-center justify-end">
+          <button
+            className="relative group inline-flex justify-center items-center rounded-2xl py-2 px-4 text-sm font-semibold w-auto m-0 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-custom text-white hover:bg-hover-custom hover:font-bold active:bg-hover-custom active:text-slate-300 focus-visible:bg-hover-custom"
+            type="button"
+            onClick={onNextStepHandler}
+          >
+            <span className="flex mr-2">Siguiente</span>
+            <ArrowLongRightIcon className="fill-white group-active:fill-current w-5 h-6" />
+          </button>
+        </div>
+      </Card>
     </>
   )
 }
--- frontend/src/layouts/classify/index.tsx
+++ frontend/src/layouts/classify/index.tsx
@@ -6,8 +6,7 @@ import { Container } from '@components/Container'
 import { EnrollmentForm } from '@components/classify/Form/EnrollmentForm'
 import { MultiStep } from '@components/classify/Form/MultiStep'
 import { GradesForm } from '@components/classify/Form/GradesForm'
-import { Card } from '@components/Card'
-import { ArrowLongRightIcon, ArrowLongLeftIcon, PaperAirplaneIcon, Squares2X2Icon } from '@heroicons/react/20/solid'
+import { Squares2X2Icon } from '@heroicons/react/20/solid'
 import Link from 'next/link'
 
 const data = {
@@ -69,7 +68,7 @@ export function ClassifyStudentLayout(): React.JSX.Element {
           href="/"
           className="relative group inline-flex justify-center items-center rounded-2xl py-2 px-4 text-sm font-semibold w-auto m-0 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-custom text-white hover:bg-hover-custom hover:text-slate-100 active:bg-hover-custom active:text-slate-300 focus-visible:bg-hover-custom"
         >
-          <Squares2X2Icon className="fill-custom group-active:fill-current w-6 h-6" />
+          <Squares2X2Icon className="fill-white group-active:fill-current w-5 h-5" />
           <span className="flex ml-3">Dashboard</span>
         </Link>
       </div>
@@ -81,39 +80,10 @@ export function ClassifyStudentLayout(): React.JSX.Element {
               <div className="w-full lg:ml-16">
                 <form className="mb-2" action="/" ref={form}>
                   <div className={classNames(active === 0 ? 'flex' : 'hidden')}>
-                    <Card title={data.enrollment.title} subtitle={data.enrollment.subtitle}>
-                      <EnrollmentForm />
-                      <div className="mt-10 flex items-center justify-end">
-                        <button
-                          className="relative group inline-flex justify-center items-center rounded-2xl py-2 px-4 text-sm font-semibold w-auto m-0 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-custom text-white hover:bg-hover-custom hover:font-bold active:bg-hover-custom active:text-slate-300 focus-visible:bg-hover-custom"
-                          onClick={onNextStepHandler}
-                        >
-                          <span className="flex mr-2">Siguiente</span>
-                          <ArrowLongRightIcon className="fill-white group-active:fill-current w-5 h-6" />
-                        </button>
-                      </div>
-                    </Card>
+                    <EnrollmentForm onNextStepHandler={onNextStepHandler} />
                   </div>
                   <div className={classNames(active === 1 ? 'flex' : 'hidden')}>
-                    <Card title={data.grades.title} subtitle={data.grades.subtitle}>
-                      <GradesForm />
-                      <div className="mt-10 flex items-center justify-between flex-wrap">
-                        <button
-                          className="relative group inline-flex justify-center items-center rounded-2xl py-2 px-4 text-sm font-semibold w-auto m-0 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-gray-200 text-gray-600 hover:bg-search hover:font-bold active:bg-search active:text-slate-300 focus-visible:bg-search"
-                          onClick={onPreviousStepHandler}
-                        >
-                          <ArrowLongLeftIcon className="fill-gray-600 group-active:fill-current w-5 h-6" />
-                          <span className="flex ml-2">Anterior</span>
-                        </button>
-                        <button
-                          className="relative group inline-flex justify-center items-center rounded-2xl py-2 px-4 text-sm font-semibold w-auto m-0 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-custom text-white hover:bg-hover-custom hover:font-bold active:bg-hover-custom active:text-slate-300 focus-visible:bg-hover-custom"
-                          onSubmit={onSubmitHandler}
-                        >
-                          <span className="flex mr-2">Clasificar</span>
-                          <PaperAirplaneIcon className="fill-white group-active:fill-current w-5 h-6" />
-                        </button>
-                      </div>
-                    </Card>
+                    <GradesForm onPreviousStepHandler={onPreviousStepHandler} onSubmitHandler={onSubmitHandler} />
                   </div>
                 </form>
               </div>
