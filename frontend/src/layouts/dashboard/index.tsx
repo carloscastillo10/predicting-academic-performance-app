69d15be705bf5693fee46608d4f1ecc7faed7bc1
Carlos Castillo <carloscastillo090916@gmail.com>
2023-07-30 10:31:04 -0500

    perf: :wheelchair: Update title links

--- frontend/src/layouts/dashboard/index.tsx
+++ frontend/src/layouts/dashboard/index.tsx
@@ -15,6 +15,7 @@ export function DashboardLayout(): React.JSX.Element {
         <Link
           href="classify/"
           className="fixed z-50 bottom-[1.875rem] m-auto inset-x-0 w-14 h-14 group inline-flex justify-center items-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 bg-custom text-white hover:bg-hover-custom hover:text-slate-100 active:bg-hover-custom active:text-slate-300 focus-visible:bg-hover-custom md:relative md:rounded-2xl md:w-auto md:h-10 md:bottom-0 md:m-0"
+          title="Classify Student"
         >
           <UserPlusIcon className="w-8 h-8 flex-none fill-custom group-active:fill-current md:w-6 md:h-6" />
           <span className="ml-3 hidden md:flex">Clasificar nuevo estudiante</span>
