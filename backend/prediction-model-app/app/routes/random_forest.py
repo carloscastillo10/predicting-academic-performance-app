14e8e61d38305f97371c4289f8eaed6ed3adacaf
Carlos Castillo <carloscastillo090916@gmail.com>
2023-08-02 17:14:12 -0500

    feat(classify-student): Add prediction service

--- backend/prediction-model-app/app/routes/random_forest.py
+++ backend/prediction-model-app/app/routes/random_forest.py
@@ -1,18 +1,14 @@
 from . import blueprint
+from app.services.random_forest import RandomForestService
 from flask import jsonify, request
 
 
 @blueprint.route('/predict', methods=['POST'])
 def predict():
-    print(request.json)
-    response = {
-        'status': 'aprobado',
-        'rules': [
-            {
-                'name': 'apeb2_ponderado <= 30.2',
-                'output': True,
-                'value': 20.2
-            }
-        ]
-    }
-    return jsonify(response), 201
+    try:
+
+        random_forest_service = RandomForestService(request.json)
+        response = random_forest_service.predict()
+        return jsonify(response), 201
+    except Exception as e:
+        return jsonify({"message": e.__str__(), "statusCode": 400}), 400
