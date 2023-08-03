11ff4096e533eaa86f673bbd3f2283be4600294f
Carlos Castillo <carloscastillo090916@gmail.com>
2023-08-03 18:25:50 -0500

    feat(classify-student): :sparkles: Adding classify student endpoint

--- backend/student-classification-microservice/src/api/students/services/student.service.ts
+++ backend/student-classification-microservice/src/api/students/services/student.service.ts
@@ -8,16 +8,18 @@ export class StudentService {
   async create(data: ClassifyStudentDto) {
     try {
       const classifyStudentData = plainToClass(ClassifyStudentDto, data)
-      await validateOrReject(classifyStudentData)
-    } catch (errors: any) {
-      const error = new ValidationError()
-      const errorPayload = errors.map((error: any) => ({
-        property: error.property,
-        constraints: error.constraints,
-      }))
-      error.target
-      error.children = errorPayload
-      throw error
+
+      // await validateOrReject(classifyStudentData)
+    } catch (errors) {
+      console.log(errors)
+      // const error = new ValidationError()
+      // const errorPayload = errors.map((error: any) => ({
+      //   property: error.property,
+      //   constraints: error.constraints,
+      // }))
+      // error.target
+      // error.children = errorPayload
+      // throw error
     }
   }
 }
