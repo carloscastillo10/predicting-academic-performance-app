9c8c0be734d29aca0ebf7ef89c7f9ead04a1b26e
Carlos Castillo <carloscastillo090916@gmail.com>
2023-08-05 15:19:19 -0500

    fix(classify-student): :bug: Fix merge conflicts

--- backend/prediction-model-app/app/routes/random_forest.py
+++ backend/prediction-model-app/app/routes/random_forest.py
@@ -6,9 +6,16 @@ from flask import jsonify, request
 @blueprint.route('/predict', methods=['POST'])
 def predict():
     try:
+<<<<<<< HEAD
+=======
+
+>>>>>>> 14e8e61 (feat(classify-student): Add prediction service)
         random_forest_service = RandomForestService(request.json)
         response = random_forest_service.predict()
         return jsonify(response), 201
     except Exception as e:
         return jsonify({"message": e.__str__(), "statusCode": 400}), 400
+<<<<<<< HEAD
         
+=======
+>>>>>>> 14e8e61 (feat(classify-student): Add prediction service)
